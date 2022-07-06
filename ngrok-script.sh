cf4d141b0f3ef2a5541a35bc329143bbadf5b04c
Anas Mahmoud <ansas4565@gmail.com>
2022-07-06 08:55:31 +0300

    Create ngrok-script.sh

---
+++ ngrok-script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export API_KEY=args[1]
+# to get the tunnel public url and port  
+python3 -c "import ngrok;client=ngrok.Client('$API_KEY');[print(i) for i in client.tunnels.list() ]" | egrep 'public_url.*started_at'  --only-matching | cut -c 15-|rev|cut -c 15-|rev
+
+# TODO
+# add the feature to create anonymous ngrok account
