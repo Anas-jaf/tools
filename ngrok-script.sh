5b84373ec80a5330a0d3d877b63a326653a665d0
Anas Mahmoud <ansas4565@gmail.com>
2022-07-07 09:33:22 +0300

    added api key function

--- ngrok-script.sh
+++ ngrok-script.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ -e apikey.txt];then
+    API_KEY=`cat apikey.txt`
+else
+    read -p "Please type your ngrok api" API_KEY
+    echo API_KEY > apikey.txt
+
 export API_KEY=args[1]
 # to get the tunnel public url and port  
 python3 -c "import ngrok;client=ngrok.Client('$API_KEY');[print(i) for i in client.tunnels.list() ]" | egrep 'public_url.*started_at'  --only-matching | cut -c 15-|rev|cut -c 15-|rev
