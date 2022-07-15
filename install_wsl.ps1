43cd315431cf53a2d0d1ba64ed97c4de777af901
Anas Mahmoud <ansas4565@gmail.com>
2022-07-15 20:02:01 +0300

    #

--- install_wsl.ps1
+++ install_wsl.ps1
@@ -1,9 +1,17 @@
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
-wsl --install
+
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+
+# wsl --install
+# kali
+
+Enable-WindowsOptionalFeature -Online -FeatureName microsoft-windows-subsystem-linux
+iwr -Uri https://aka.ms/wsl-kali-linux-new -OutFile  Kali.appx -UseBasicParsing
+Add-AppxPackage .\Kali.appx
 
 kali
-
 sudo wget https://archive.kali.org/archive-key.asc -O /etc/apt/trusted.gpg.d/kali-archive-keyring.asc
 --2022-07-15 09:42:39--  https://archive.kali.org/archive-key.asc
 
